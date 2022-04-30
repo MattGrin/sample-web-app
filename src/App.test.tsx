e5a6d221d24f625a76f17194e162de248e260e29
MattGrin <matias.verdejo@gmail.com>
2022-04-30 16:15:23 -0400

    feat(scripts): added github ci test coverage script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('render app core', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Sample web app content");
-  expect(linkElement).toBeInTheDocument();
+describe('render app core', () => {
+  it('should have header text', () => {
+    render(<App />);
+    const headerElement = screen.getByText("Sample web app");
+    const contentElement = screen.getByText("Sample web app content");
+    expect(headerElement).toBeInTheDocument();
+    expect(contentElement).toBeInTheDocument();
+  })
 });
