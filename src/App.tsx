2a439786529b8708752c478f588cf4e9040c001b
MattGrin <matias.verdejo@gmail.com>
2022-04-30 17:33:21 -0400

    feat(app-root): added grid screen layers control

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
   return (
     <div className="core-container">
-      <header className="layout-header">
-        <h1>
-          Sample web app
-        </h1>
-      </header>
-      <section>
-        Sample web app content
-      </section>
+      {/*
+        * Every core-container child will float over the other child, 
+        * Here you can add overlays like dialogs, popups, modals, etc.
+        */}
+      <Layout>
+        <Layout.Header>Sample web app</Layout.Header>
+        <Layout.Content>Sample web app content</Layout.Content>
+        <Layout.StickyFooter>Sample web app footer</Layout.StickyFooter>
+      </Layout>
     </div>
   );
 }
