bc03e08446e438012cd6b5b108e7cbd9de5c18ef
MattGrin <matias.verdejo@gmail.com>
2022-04-30 15:18:07 -0400

    chore(test): updated test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render app core', () => {
   render(<App />);
   const linkElement = screen.getByText("Sample web app content");
   expect(linkElement).toBeInTheDocument();
