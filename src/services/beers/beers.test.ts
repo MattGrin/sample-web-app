84c3dd0c061ff5e7d3b8a9a93f41e31944fa5808
MattGrin <matias.verdejo@gmail.com>
2022-04-30 21:35:30 -0400

    feat(beers-service): updated tests to increase coverage

--- src/services/beers/beers.test.ts
+++ src/services/beers/beers.test.ts
@@ -8,13 +8,43 @@ describe('Beers service', () => {
     expect(receivedService).toBeDefined();
   });
 
-  it('initialize service return BeersService instance', () => {
+  it('initialize service return BeersService instance', async () => {
     const fakeAxiosInstance = {
-      get: jest.fn()
+      get: (a: any) => new Promise((resolve) => resolve(a)),
     } as unknown as AxiosInstance
 
     const beersService = initBeersService(fakeAxiosInstance);
     expect(beersService).toHaveProperty('getBeers');
     expect(beersService).toHaveProperty('findBeer');
+
+    expect(beersService.getBeers).toBeInstanceOf(Function);
+    expect(beersService.findBeer).toBeInstanceOf(Function);
+
+    const someBeersResponseArgs = {
+      page: 1,
+      perPage: 2,
+    };
+
+    const someBeersPromise = beersService.getBeers(someBeersResponseArgs)
+
+    const someFoundBeerResponseArgs = {
+      name: 'some beer name',
+    };
+
+    const someFoundBeerPromise = beersService.findBeer(someFoundBeerResponseArgs)
+
+    expect(someBeersPromise).toBeInstanceOf(Promise);
+
+    const someBeersResponse = await someBeersPromise
+    const sanitizedSomeBeerRes = `?page=${someBeersResponseArgs.page}&per_page=${someBeersResponseArgs.perPage}`
+    expect(someBeersResponse).toEqual(sanitizedSomeBeerRes);
+
+    expect(someFoundBeerPromise).toBeInstanceOf(Promise);
+    const sanitizedSomeFoundBeerRes = `?beer_name=${someFoundBeerResponseArgs.name.replaceAll(' ', '_')}`
+
+    const someFoundBeerResponse = await someFoundBeerPromise
+    expect(someFoundBeerResponse).toEqual(sanitizedSomeFoundBeerRes);
+
+    //})
   });
 })
