562f9b0a7dbb3732aabe3a64f8514df1894b2102
MattGrin <matias.verdejo@gmail.com>
2022-05-01 18:03:28 -0400

    feat(beers-service): updated services URL

--- src/services/beers/beers.ts
+++ src/services/beers/beers.ts
@@ -3,13 +3,17 @@ import { BeersService } from './beers.types';
 
 
 const initBeersService: Service<BeersService> = (httpClient) => {
+
+  const BEERS_API_URL = 'beers'
   /**
    * Get List of BeersItems
    */
   const getBeers: BeersService['getBeers'] = ({
     page = 1,
     perPage = 10,
-  }) => httpClient.get(`?page=${page}&per_page=${perPage}`);
+  }) => httpClient.get(`${BEERS_API_URL
+    }?page=${page}&per_page=${perPage}`)
+
 
   /**
    * Get list of matched BeerItems
@@ -18,7 +22,7 @@ const initBeersService: Service<BeersService> = (httpClient) => {
     name,
   }) => {
     const sanitizedSearchNameParam = name.replace(/\s/g, '_');
-    return httpClient.get(`?beer_name=${sanitizedSearchNameParam}`);
+    return httpClient.get(`${BEERS_API_URL}?beer_name=${sanitizedSearchNameParam}`);
   }
 
   return {
