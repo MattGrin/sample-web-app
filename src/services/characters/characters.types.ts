516ebba749fe045e79cc5642b0659ea7a2c52448
Matias Verdejo <matias.verdejo@cornershopapp.com>
2022-05-02 15:36:01 -0400

    feat(characters-service): added type definition

--- src/services/characters/characters.types.ts
+++ src/services/characters/characters.types.ts
@@ -1,12 +1,38 @@
 import { ServerRes } from "../../utils/utils.types";
 
+interface Linkable {
+  name: string,
+  url: string
+}
+
 export interface Character {
-  id: number;
+  id: number,
+  name: string,
+  status: string,
+  species: string,
+  type: string,
+  gender: string,
+  origin: Linkable,
+  location: Linkable,
+  image: string,
+  episode: string[],
+  url: string,
+  created: string
+}
+
+interface ResponsePaginationMeta {
+  count: number,
+  pages: number,
+  next: string | null,
+  prev: string | null,
 }
 
 export type CharacterList = Character[]
 
-export type CharactersAPI = CharacterList
+export interface CharactersAPI {
+  info: ResponsePaginationMeta,
+  results: CharacterList
+}
 
 export interface CharactersService {
   getCharacters: (params: {}) => ServerRes<CharactersAPI>,
