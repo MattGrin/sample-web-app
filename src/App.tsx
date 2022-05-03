08f4854a2a0b4db16c2e4d8dbf53fe10f12511ba
Matias Verdejo <matias.verdejo@cornershopapp.com>
2022-05-03 03:02:02 -0400

    feat(app): imported CharactersScreen as full layout element

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CharactersScreen from "./views/characters";
 
 function App() {
   return (
     <div className="core-container">
       {/*
-        * Every core-container child will float over the other child, 
-        * Here you can add overlays like dialogs, popups, modals, etc.
-        */}
-      <Layout>
-        <Layout.Header>Sample web app</Layout.Header>
-        <Layout.Content>Sample web app content</Layout.Content>
-        <Layout.StickyFooter>Sample web app footer</Layout.StickyFooter>
-      </Layout>
+       * Every core-container child will float over the other child,
+       * Here you can add overlays like dialogs, popups, modals, etc.
+       */}
+      <CharactersScreen />
     </div>
   );
 }
