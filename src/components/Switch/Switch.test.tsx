411fce3eb03618b3b250cadfba58dc5ed4058e17
Matias Verdejo <matias.verdejo@cornershopapp.com>
2022-05-03 07:30:09 -0400

    feat(switch-tests): updated smoke tests on switch

--- src/components/Switch/Switch.test.tsx
+++ src/components/Switch/Switch.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Switch from "./Switch";
 
-describe("SoftHeader", () => {
-  it("renders without crashing", () => {
-    render(<Switch.Wrapper label="test">test</Switch.Wrapper>);
-    const foundElement = screen.getByTestId("switch-wrapper");
+describe("Switch", () => {
+  it("renders wrapper without crashing", () => {
+    render(<Switch.Wrapper label="test">test children</Switch.Wrapper>);
+    const foundElement = screen.getByText("test children");
+    expect(foundElement).toBeInTheDocument();
+  });
+
+  it("renders item without crashing", () => {
+    render(
+      <Switch.Item isActive={false} onClick={() => {}} aria-label="test">
+        value
+      </Switch.Item>
+    );
+    const foundElement = screen.getByText("value");
     expect(foundElement).toBeInTheDocument();
   });
 });
