b29546d00381be615c94819912bdb466b51e10ce
Matias Verdejo <matias.verdejo@cornershopapp.com>
2022-05-03 03:00:14 -0400

    feat(utils-types): added react dispatcher shortcut

--- src/utils/utils.types.ts
+++ src/utils/utils.types.ts
@@ -3,3 +3,5 @@ import { AxiosResponse, AxiosInstance } from 'axios';
 export type ServerRes<T> = Promise<AxiosResponse<T>>;
 
 export type Service<T> = (httpClient: AxiosInstance) => T;
+
+export type ReactDispatcher<T> = React.Dispatch<React.SetStateAction<T>>;
