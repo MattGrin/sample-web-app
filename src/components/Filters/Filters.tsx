c63d6b1a3d3d2e160b1a0cc17502f16f2ea9f03f
Matias Verdejo <matias.verdejo@cornershopapp.com>
2022-05-03 02:59:22 -0400

    feat(filters): filters refactor logic and new components added

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,90 +1,138 @@
-import { useState } from 'react';
-import classnames from 'classnames';
-import { Character } from '../../services/characters/characters.types';
-import './Filters.css'
+import { useState } from "react";
+import classnames from "classnames";
+import { Character } from "../../services/characters/characters.types";
+import "./Filters.css";
+import { ReactDispatcher } from "../../utils/utils.types";
 
 export interface FilterProps {
-  onSearchByName?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  loading: boolean;
+  setName: ReactDispatcher<string>;
+  setStatus: ReactDispatcher<Character["status"] | null>;
+  setGender: ReactDispatcher<Character["gender"] | null>;
+  name: string;
+  status: Character["status"] | null;
+  gender: Character["gender"] | null;
+  handleClearFilter: () => void;
 }
 
+const TextInput: React.FC<React.HTMLProps<HTMLInputElement>> = (inputProps) => {
+  return (
+    <input type="text" maxLength={30} className="text-input" {...inputProps} />
+  );
+};
 
-const TextInput: React.FC<React.HTMLAttributes<HTMLInputElement>> = (inputProps) => {
-  return <input type="text" maxLength={30} className="text-input" {...inputProps}/>
-}
-
-
-const PrimaryButton: React.FC<React.HTMLAttributes<HTMLButtonElement>> = ({children, ...buttonProps}) => {
-  return <button className="primary-button" {...buttonProps}>{children}</button>
-}
+const PrimaryButton: React.FC<React.HTMLAttributes<HTMLButtonElement>> = ({
+  children,
+  ...buttonProps
+}) => {
+  return (
+    <button className="primary-button" {...buttonProps}>
+      {children}
+    </button>
+  );
+};
 
 /**
  * Rick and morty data base
  * Soft header
  */
 const Filters = ({
-  onSearchByName,
+  loading,
+  setName,
+  setStatus,
+  setGender,
+  name,
+  status,
+  gender,
+  handleClearFilter,
 }: FilterProps) => {
-
   const [expandedView, setExpandedView] = useState(false);
-  const [activeStatus, setActiveStatus] = useState<Character['status'] | null>(null);
-  const availableStatus: Character['status'][] = ['alive', 'dead', 'unknown'];
+  const availableStatus: Character["status"][] = ["alive", "dead", "unknown"];
+  const availableGenders: Character["gender"][] = [
+    "female",
+    "male",
+    "genderless",
+    "unknown",
+  ];
 
-  const [activeGender, setActiveGender] = useState<Character['gender'] | null>(null);
-  const availableGenders: Character['gender'][] = ['female', 'male', 'genderless', 'unknown']
+  const toggleExpnandedView = () => setExpandedView((e) => !e);
 
-  return <section data-testid="filters" className='filters'>
-    <section data-testid="visible-section" className="filters__visible-section" >
-      <TextInput 
-        aria-label='search by name'
-        placeholder='Search by name...'
-        onChange={onSearchByName}
-      />
-      <PrimaryButton className="primary-button" onClick={() => setExpandedView(e => !e)}>
-        <h4>
-          icon
-        </h4>
-      </PrimaryButton>
+  return (
+    <section data-testid="filters" className="filters">
+      <section
+        data-testid="visible-section"
+        className="filters__visible-section"
+      >
+        {loading && <h1 className="loader">Loading...</h1>}
+        <TextInput
+          aria-label="search by name"
+          placeholder="Search by name..."
+          value={name}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setName(event.target.value);
+          }}
+        />
+        <PrimaryButton className="primary-button" onClick={toggleExpnandedView}>
+          <h4>X</h4>
+        </PrimaryButton>
+      </section>
+      {expandedView && (
+        <>
+          <section className="filters__collapsable-area">
+            <label htmlFor="status-selector">
+              <b>Status</b>
+            </label>
+            <div
+              id="status-selector"
+              aria-label={`${
+                status ? status + "is selected" : "no status is selected"
+              }`}
+              className="switch"
+            >
+              {availableStatus.map((_status) => (
+                <div
+                  onClick={() => setStatus(_status)}
+                  aria-label={`${_status} status option`}
+                  className={classnames("switch__item", {
+                    "switch__item--active": status === _status,
+                  })}
+                >
+                  {_status}
+                </div>
+              ))}
+            </div>
+            <label htmlFor="gender-selector">
+              <b>Gender</b>
+            </label>
+            <div
+              id="gender-selector"
+              aria-label={`${
+                gender ? gender + "is selected" : "no gender is selected"
+              }`}
+              className="switch"
+            >
+              {availableGenders.map((_gender) => (
+                <div
+                  onClick={() => setGender(_gender)}
+                  aria-label={`${_gender} gender option`}
+                  className={classnames("switch__item", {
+                    "switch__item--active": gender === _gender,
+                  })}
+                >
+                  {_gender}
+                </div>
+              ))}
+            </div>
+          </section>
+          <section data-testid="filter-cleanup">
+            <PrimaryButton onClick={handleClearFilter}>
+              <h4>clear filters</h4>
+            </PrimaryButton>
+          </section>
+        </>
+      )}
     </section>
-    {expandedView && <section className="filters__collapsable-area">
-      <label htmlFor='status-selector'><b>Status</b></label>
-      <div
-        id="status-selector"
-        aria-label={`${activeStatus ? activeStatus + 'is selected': 'no status is selected'}`}
-        className='switch'
-      >
-        {availableStatus.map(status => <div 
-          onClick={() => setActiveStatus(status)}
-          aria-label={`${status} status option`}
-          className={
-            classnames(
-              "switch__item",
-              {'switch__item--active': activeStatus === status}
-            )
-          }>
-          {status}
-        </div>)
-        }
-      </div>
-      <label htmlFor='gender-selector'><b>Gender</b></label>
-      <div
-        id="gender-selector"
-        aria-label={`${activeGender ? activeGender + 'is selected': 'no gender is selected'}`}
-        className='switch'
-      >
-        {availableGenders.map(gender => <div 
-          onClick={() => setActiveGender(gender)}
-          aria-label={`${gender} gender option`}
-          className={
-            classnames(
-              "switch__item",
-              {'switch__item--active': activeGender === gender}
-            )
-          }>
-          {gender}
-        </div>)}
-      </div>
-    </section>}
-  </section>
-}
+  );
+};
 
 export default Filters;
